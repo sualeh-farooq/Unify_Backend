20c8bfab5fbdf3d0a742bf143a84e76686cd7567
Sualeh Farooq <sualehfarooq65@gmail.com>
2024-07-18 14:11:20 +0500

    fix build error

--- api/index.js
+++ api/index.js
@@ -1,24 +1,22 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const cors = require('cors')
-const authRoutes = require('../routes/auth')
-const imageRoutes = require('../routes/image_routes')
-const path = require('path')
-const multer = require('multer')
-const cookieParser = require('cookie-parser')
-const adminRoutes = require('../routes/admin')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const authRoutes = require('../routes/auth');
+const imageRoutes = require('../routes/image_routes');
+const path = require('path');
+const multer = require('multer');
+const cookieParser = require('cookie-parser');
+const adminRoutes = require('../routes/admin');
+require('dotenv').config();
+
 mongoose.connect(process.env.DATABASE_URI)
     .then(() => {
-        console.log(`Database Connected`)
-        app.listen(process.env.PORT, () => {
-            console.log('Server Started with DB')
-        })
+        console.log(`Database Connected`);
     })
     .catch((err) => {
-        console.log(err)
-    })
+        console.log(err);
+    });
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -29,34 +27,29 @@ const storage = multer.diskStorage({
     }
 });
 
- const upload = multer({ storage: storage })
+const upload = multer({ storage: storage });
 
-
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cors())
-app.use(cookieParser())
-app.use('/api/images', express.static('public/uploads'))
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(cookieParser());
+app.use('/api/images', express.static('public/uploads'));
 
 app.get('/', (req, res) => {
-    res.send(`<p>Server is running</p>`)
-})
-
+    res.send(`<p>Server is running</p>`);
+});
 
 app.use('/api/images', imageRoutes);
-app.use('/api/auth', authRoutes)
-app.use('/api/admin' , adminRoutes)
-
+app.use('/api/auth', authRoutes);
+app.use('/api/admin', adminRoutes);
 
 // Middleware function handler on every request API
 app.use((req, res, next) => {
-    console.log(`Method: ${req.method}`)
-    console.log(`Path: ${req.path}`)
-    console.log(`URL: ${req.url}`)
-    next()
-})
+    console.log(`Method: ${req.method}`);
+    console.log(`Path: ${req.path}`);
+    console.log(`URL: ${req.url}`);
+    next();
+});
 
-
-module.exports = upload
+// Export the app instance for Vercel to use
+module.exports = {app , upload}
